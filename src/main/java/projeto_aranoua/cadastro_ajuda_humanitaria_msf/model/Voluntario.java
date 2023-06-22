21773df97b134e221d085f2561c1652b7f1bbaee
leonardolimaArt <leonardolima.art@gmail.com>
2023-06-22 15:39:39 -0400

    Implementação do encapsulamento e propriedades da classe modelo volutário. ~/model/Voluntario

--- src/main/java/projeto_aranoua/cadastro_ajuda_humanitaria_msf/model/Voluntario.java
+++ src/main/java/projeto_aranoua/cadastro_ajuda_humanitaria_msf/model/Voluntario.java
@@ -1,4 +1,75 @@
 package projeto_aranoua.cadastro_ajuda_humanitaria_msf.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Voluntario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String passaporte;
+    private String nomeCompleto;
+    private String idade;
+    private String telefone;
+    private String email;
+    private String tipoSangue;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassaporte() {
+        return passaporte;
+    }
+
+    public void setPassaporte(String passaporte) {
+        this.passaporte = passaporte;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipoSangue() {
+        return tipoSangue;
+    }
+
+    public void setTipoSangue(String tipoSangue) {
+        this.tipoSangue = tipoSangue;
+    }
 }
